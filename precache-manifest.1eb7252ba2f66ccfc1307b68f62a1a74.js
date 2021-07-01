73ad85ec7a93a18cd0786e0293fa8058c7d724df
Léo <tellier.leo@hotmail.fr>
2021-07-01 11:05:33 +0200

    Updates

---
+++ precache-manifest.1eb7252ba2f66ccfc1307b68f62a1a74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f193688c4a12998676a01e83a8cdcca4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9debda667cd23fa4481e",
+    "url": "./static/js/2.f83afe66.chunk.js"
+  },
+  {
+    "revision": "ce2886c9d9d5129f5fe39c8d77b88335",
+    "url": "./static/js/2.f83afe66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888183715c8adbeec4f4",
+    "url": "./static/js/main.ac6e250b.chunk.js"
+  },
+  {
+    "revision": "8339a73111e0ba616aca",
+    "url": "./static/js/runtime-main.221952d0.js"
+  }
+]);
